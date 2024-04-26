bf5afe1e504c9a1fc739ee83880ba795429a95b8
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-04-26 08:31:55 +0300

    worked on the second question in constractors

--- constractors.js
+++ constractors.js
@@ -28,3 +28,47 @@ const smallcar = new Rental ('Volvo', 'Aisha', "2024-4-23", "2024-7-12")
 console.log({smallcar});
 
 //Two
+class Question {
+    constructor(text, options, correctAnswer) {
+        this.text = text;
+        this.options = options;
+        this.correctAnswer = correctAnswer;
+    }
+    checkAnswer(userAnswer) {
+        return userAnswer === this.correctAnswer;
+    }
+}
+const sampleQuestion = new Question(
+    "Which country is in East Africa?",
+    ["Ethiopia", "Kenya", "Madagascar", "Djibouti"],
+);
+console.log(sampleQuestion.checkAnswer("Kenya"));
+console.log(sampleQuestion.checkAnswer("kenya"));
+class Quiz {
+    constructor() {
+        this.questions = [];
+        this.currentQuestionIndex = 0;
+        this.score = 0;
+    }
+    addQuestion(question) {
+        this.questions.push(question);
+    }
+    nextQuestion() {
+        this.currentQuestionIndex++;
+    }
+    submitAnswer(userAnswer) {
+        const currentQuestion = this.questions[this.currentQuestionIndex];
+        if (currentQuestion.checkAnswer(userAnswer)) {
+            this.score++;
+        }
+        this.nextQuestion();
+    }
+}
+const quiz = new Quiz();
+const firstquestion = new Question("What is 20+5 ?", ["3", "25", "5"], "25");
+const secondquestion = new Question("Who is the parent?", ["Njiru", "Mary", "Sammy"], "Njiru");
+quiz.addQuestion(firstquestion);
+quiz.addQuestion(secondquestion);
+quiz.submitAnswer("25");
+quiz.submitAnswer("Sammy");
+console.log("Score:", quiz.score);
